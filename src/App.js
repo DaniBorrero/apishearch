03c8937bd62c762027d9b25b50b1947e914db4cf
daniBorrero <borrerodaniel89@gmail.com>
2022-09-12 21:31:06 -0300

    implementada un api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [pokemon, setPokemon] = useState()
+
+  
+
+  useEffect(() => {
+    const min = 1;
+    const max = 150;
+    const random = Math.floor(Math.random()*(max-min+1)+min);
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${random}`)
+      .then(response => response.json())
+      .then(result => {
+        // console.log(result.forms[0].name)
+        setPokemon(result.forms[0].name)
+        
+      })
+      .catch(error => console.log('error', error));
+  }, [])
+
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  fetch("api.giphy.com/v1/gifs/search?q=pikachu&limit=1&api_key=3KdB6xujDx3IGO3O3w0Ze2KIa2Y2SPsz", requestOptions)
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>{pokemon}</h1>
     </div>
   );
 }
