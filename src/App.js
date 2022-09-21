eb3a48532eac571700a41a65e9f2260cb36dbbba
daniBorrero <borrerodaniel89@gmail.com>
2022-09-21 14:48:31 -0300

    fetch working, get image and show - need to fix doble fetch to api

--- src/App.js
+++ src/App.js
@@ -1,43 +1,45 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [pokemon, setPokemon] = useState()
+  const [poke, setPoke] = useState('')
+  const [url, setUrl] = useState('');
 
-  
-
-  useEffect(() => {
-    const min = 1;
-    const max = 150;
-    const random = Math.floor(Math.random()*(max-min+1)+min);
-
-    fetch(`https://pokeapi.co/api/v2/pokemon/${random}`)
+  const apiGiphy = () => {
+    fetch(`https://api.giphy.com/v1/gifs/search?q=${poke}&api_key=3KdB6xujDx3IGO3O3w0Ze2KIa2Y2SPsz`)
       .then(response => response.json())
-      .then(result => {
-        // console.log(result.forms[0].name)
-        setPokemon(result.forms[0].name)
-        
+      .then(result => { 
+        setUrl(result.data[0].images.original.url)
       })
       .catch(error => console.log('error', error));
-  }, [])
-
-  var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
   };
-  
-  fetch("api.giphy.com/v1/gifs/search?q=pikachu&limit=1&api_key=3KdB6xujDx3IGO3O3w0Ze2KIa2Y2SPsz", requestOptions)
-    .then(response => response.json())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
 
 
+
+  useEffect(() => {
+    const getPokemon = () => {
+      let random = Math.floor(Math.random() * (400 - 1 + 1)) + 1;
+      fetch(`https://pokeapi.co/api/v2/pokemon/${random}`)
+        .then(response => response.json())
+        .then(result => {
+          setPoke(result.forms[0].name)
+        })
+        .catch(error => console.log('error', error));
+    };
+
+    getPokemon()
+  }, []);
+
   
+  apiGiphy()
+
   return (
     <div className="App">
-        <h1>{pokemon}</h1>
+      <h1>{poke}</h1>
+      <img src={url}></img>
     </div>
   );
+
 }
 
 export default App;
